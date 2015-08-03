779034bf3233c5898376572ea1d04a0ab5d46bd8
Tersken <koen.tersago@gmail.com>
2015-08-03 20:18:44 +0200

    Cms start
    Added static list and start detail

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -7,9 +7,11 @@ angular.module('MultiLangCms', ['ngRoute']).
 	  
 	 $routeProvider.
   	
-      /*when('/login', { templateUrl: 'partials/login.html', controller: LoginCtrl }).
-      when('/logout', { templateUrl: 'partials/login.html', controller: LoginCtrl }).*/
-      when('/', { templateUrl: 'partials/home.html', controller: BaseCtrl }).
+         /*when('/login', { templateUrl: 'partials/login.html', controller: LoginCtrl }).
+         when('/logout', { templateUrl: 'partials/login.html', controller: LoginCtrl }).*/
+         when('/', { templateUrl: 'partials/home.html', controller: BaseCtrl }).
+         when('/cms', { templateUrl: 'partials/cms/list.html', controller: tettenCtrl }).
+         when('/cms/page/:id', { templateUrl: 'partials/cms/detail.html', controller: tetten2Ctrl }).
      // when('/faceBookNextStep', { templateUrl: 'partials/faceBookNextStep.html', controller: FacebookController }).
      // when('/googleNextStep', { templateUrl: 'partials/googleNextStep.html', controller: GoogleController }).
 
--- frontend/js/controllers/baseController.js
+++ frontend/js/controllers/baseController.js
@@ -9,5 +9,50 @@ function BaseCtrl($scope, $location, $filter, $timeout, $http) {
 	$scope.init();
 }
 
+function tettenCtrl($scope, $location, $filter, $timeout, $http) {
+	$scope.init = function() {
+		var selected = [];
+		var dataSet = [
+			{
+				"DT_RowId": "row_1",
+				"id" : 1,
+				'title': 'Frocio'
+			},
+			{
+				"DT_RowId": "row_2",
+				"id" : 2,
+				'title': 'Kwattaman'
+			}
+
+		];
+			$('#cmsPageList').dataTable( {
+				"data": dataSet,
+				"columns": [
+					{ "data": "id" , "title": '#', "class": 'idColumn'},
+					{ "data": "title", "title": 'Title'},
+				]
+			} );
+			$('#cmsPageList tbody').on('click', 'tr', function () {
+				var id = this.id;
+				var idSplit = id.split('_');
+				console.log(idSplit);
+				window.location = '#/cms/page/'+ idSplit[1];
+			} );
+	};
+
+	$scope.init();
+}
+
+function tetten2Ctrl($scope, $location, $filter, $timeout, $http, $routeParams) {
+	$scope.init = function() {
+		console.log($routeParams.id);
+		$scope.id = $routeParams.id;
+	};
+
+	$scope.init();
+}
+
+
+
 
 
